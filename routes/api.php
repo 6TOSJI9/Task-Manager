83dacb93dca1ddecdfbf7c56dee3a86c2efec250
TOSHI <150638768+6TOSJI9@users.noreply.github.com>
2024-09-27 19:59:54 +0600

    first

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
 
-
+Route::post('register', [RegisteredUserController::class, 'store']);
+Route::post('login', [AuthenticatedSessionController::class, 'store']);
+Route::post('logout', [AuthenticatedSessionController::class, 'destroy']);
 Route::middleware(['auth:sanctum'])->group(function () {
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
